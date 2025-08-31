a82fa700b1fc177458b8902600592edc0a7bb372
muslima <rasulova22muslima@gm.com>
2025-08-31 13:06:02 +0500

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var name = prompt("Как вас зовут")
+console.log("Вас зовут " + name)
+var age = prompt("Сколько вам лет?")
+console.log("Ваш возраст " + age);
+
+let answer1 = prompt("Решите пример 10 + 5")
+console.log("Пример 1: " + "10 + 5 = " + (10 + 5) + " Ваш ответ " + answer1)
+
+let answer2 = prompt("Решите пример 10 - 5")
+console.log("Пример 2: " + "10 - 5 = " + (10 - 5) + " Ваш ответ " + answer2)
+
+let answer3 = prompt("Решите пример 10 * 5")
+console.log("Пример 3: " + "10 * 5 = " + (10 * 5) + " Ваш ответ " + answer3)
+
+let answer4 = prompt("Решите пример 10 / 5")
+console.log("Пример 4: " + "10 / 5 = " + (10 / 5) + " Ваш ответ " + answer4)
+
+let answer5 = prompt("Решите пример 10 % 5")
+console.log("Пример 5: " + "10 % 5 = " + (10 % 5) + " Ваш ответ " + answer5)
+
+
+var X = 20
+var Y = 30
+var Z = 40
+prompt("Найдите среднее арифметическое значение " + X + "," + Y + "," + Z + ",")
+console.log("Среднее арифметическое значение " + (X + Y + Z)) 
